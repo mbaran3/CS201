2090c0348c9dc26432ed64f802e00cb62e26bc1c
Michael baran <mbaran3@alaska.edu>
2020-08-28 10:50:44 -0800

    HW0 Code mostly finished still need to write a poem

--- HW0/Source.cpp
+++ HW0/Source.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+using namespace std;
+
 
 int main() {
+	int z;
+	cout << "Enter 1 for Hello Wolrd. \nEnter 2 for a poem. \nEnter 3 for my poem \n";
+		cin >> z;
+		if (z == 1) {
+			int a;
+			cout << "How many time would you like to pint Hellow Wolrd?\n";
+			cin >> a;
+			for (int i = 0; i < a; i++) {
+				cout << "Hello, Wolrd \n";
+			}
 
-	std::cout << "Hello World" << std::endl;
+	}
+		if (z == 2) {
+			cout << "'A World of Dew' by Kobayshi Issa " << endl;
+			cout << "A world of dew,\nAnd within every dewdrop,\nA world of struggle." << endl;
+		}
+
+		if (z == 3) {
+			cout << "Works" << endl;
+		}
+
+		cout << "Press ENTER to quit";
 	return 0;
 }
