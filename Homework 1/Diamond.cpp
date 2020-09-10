bd9ca99587b3de5296d0144c6a0c72d1dcb45a82
Michael baran <mbaran3@alaska.edu>
2020-09-10 06:14:07 -0800

    Wrote code for the top side of the diamond

--- Homework 1/Diamond.cpp
+++ Homework 1/Diamond.cpp
@@ -4,3 +4,28 @@ Michael Baran
 2 Sept 2020
 HW0 for CS 201
 */
+#include <iostream>
+
+using std::endl;
+using std::cout;
+using std::cin;
+
+int main() {
+	int n = 5;
+	int space = n - 1;
+	
+	for (int i = 0; i < n; i++) {
+		
+		for (int j = 0; j < space; j++) {
+			cout << "_";
+
+		}
+		for (int k = 0; k <= i; k++) {
+				cout << "# ";
+		}
+		
+		cout << endl;
+		space--;
+	}
+	return 0;
+}
