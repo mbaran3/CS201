a6a11a9efa129a965c85b73c77aa12da9807d1ac
Michael baran <mbaran3@alaska.edu>
2020-10-09 17:55:19 -0800

    completed randomgenerator() in bullsandcows.cpp and added more information for the user

--- HW4/bullsandcows/bulls-and-cows.cpp
+++ HW4/bullsandcows/bulls-and-cows.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <time.h>
 #include <sstream>
-
+//takes the users guess and turns it into a vector
 void getguess(std::vector<int>& vGuess) {
 	bool run = true;
 	std::string guess;
@@ -16,13 +16,18 @@ void getguess(std::vector<int>& vGuess) {
 			guess.insert(3, " ");
 			guess.insert(5, " ");
 			std::istringstream in(guess);
-			for (int i = 0; i < vGuess.size(); i++)
+			for (int i = 0; i < vGuess.size(); i++) {
 				in >> vGuess[i];
+				if(!in)
+					std::cout << "Invalid input please only use numbers\n";
+			}
+
 			return;
 		}
 		std::cout << "Please enter a valid input\n";
 	}
 }
+//checks the guess and returns true if the players won the game
 bool checkguess(std::vector<int> Numbers, std::vector<int> Guess) {
 		int bull = 0;
 		int cow = 0;
@@ -39,23 +44,30 @@ bool checkguess(std::vector<int> Numbers, std::vector<int> Guess) {
 		std::cout << "You have " << bull << " Bulls and " << cow << " Cows\n";
 		return false;
 }
-void randomgenerator(std::vector<int> Numbers) {
-	srand(time(NULL));
-	for (int i = 0; i < Numbers.size(); i++) {
-		int j = rand() % 10 + 1;
+//uses time.h to generate random numbers for vector Numbers
+void randomgenerator(std::vector<int>& Numbers) { 
+	srand(time(NULL));						
+	for (int i = 0; i < 4; i++) {
+		int num = rand() % 10 + 1;
+		Numbers[i] = num;
 	}
 }
 int main() {
 	std::vector<int> vGuess(4);
-	std::vector<int> Numbers = { 1, 2, 3, 4 };
-	getguess(vGuess);
+	std::vector<int> Numbers(4);
+	randomgenerator(Numbers);
+	std::cout << "A bull is a number right in the right position.\n";
+	std::cout << "A cow is a number right in the wrong position.\n";
+	std::cout << "Your goal is the guess the right 4 digit number\n";
+
+	std::cout << "\n\n";
 	bool run = true;
 	while (run) {
 		getguess(vGuess);
 		if (checkguess(Numbers, vGuess))
 			break;
 	}
-	std::cout << "You win";
+	std::cout << "You win!!";
 	
 	return 0;
 }
