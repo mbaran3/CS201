4b344ded0593b54075ad148ddf1e77e2d03d424b
Michael baran <mbaran3@alaska.edu>
2020-11-11 21:03:10 -0900

    Add AddtoCart(), PrintStore()

--- HW6/shopping/shopping.cpp
+++ HW6/shopping/shopping.cpp
@@ -6,18 +6,44 @@
 
 
 struct Record {
+
 	double UnitPrice;
 	int Units;
 	std::string discription;
 };
 struct Cart {
 
-	double UnitPrice;
+	double TotalPrice;
 	int Units;
 
 };
 
-void AddtoRecord (std::string itemname, std::map<std::string, Record>& Items){
+void AddtoCart(std::map < std::string, Record> Record,
+	std::map<std::string, Cart>& Cart, std::string item) {
+
+	bool check = false;
+
+	while (!check) {
+		int UnitstoBuy;
+		std::string str;
+		std::cout << "How many " << item << " would you like to buy\n";
+		std::getline(std::cin, str);
+		std::stringstream in(str);
+		in >> UnitstoBuy;
+		if (!in)
+			std::cout << "Please Enter a Number";
+		else {
+			Cart[item].Units = UnitstoBuy;
+			Cart[item].TotalPrice = Record[item].UnitPrice * UnitstoBuy;
+			break;
+		}
+	}
+	std::cout << "You now have " << Cart[item].Units << " of " << "item"
+		<< "For a total price of " << Cart[item].TotalPrice;
+	return;
+}
+
+void AddtoRecord(std::string itemname, std::map<std::string, Record>& Items) {
 	bool check = false;
 
 	std::cout << "Enter the Unit Price\n";
@@ -48,6 +74,7 @@ void AddtoRecord (std::string itemname, std::map<std::string, Record>& Items){
 			std::cout << "Please Enter a Number\n";
 		else {
 			Items[itemname].Units = Units;
+			break;
 		}
 	}
 
@@ -68,7 +95,8 @@ void PrintStore(std::map< std::string, Record>& Store) {
 		[](const std::pair<std::string, Record> myStore) {
 			std::cout << myStore.first << " There are " << myStore.second.Units << " in stock"
 				<< " for " << myStore.second.UnitPrice << "\n";
-			std::cout << "Item discription\n";
+			std::cout << "Item discription: ";
+			std::cout << myStore.second.discription << std::endl;
 		});
 }
 
@@ -78,6 +106,9 @@ int main() {
 	std::map < int, Cart> Cart;
 	std::map < std::string, Record> Items;
 	AddtoRecord("Mustard", Items);
+	PrintStore(Items);
+	std::cout << "Type the name of something if you want to buy it"
+		<< "Type Leave to Leave the Store";
 
 	return 0;
 }
